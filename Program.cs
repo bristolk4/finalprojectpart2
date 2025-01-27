824964ce85313a6e7aa85e59ae42cd2601b4679e
Kayla Bristol <bristolk4@gmail.com>
2025-01-27 13:55:55 -0500

    added more comments

--- Program.cs
+++ Program.cs
@@ -4,8 +4,11 @@ class Program
     static void Main(string[] args)
     {
         int score = 0;
+        // Initial score set to 0
         bool gameOn = true;
+        // Initial state of game loop set to true, stops game if turned to false
         ConsoleKey? lastKey = null;
+        // Tracks the last key user typed. The ? makes ConsoleKey a nullable type which means it can either take the value of a key or have no value. It is ititially null because the user hasn't typed anything when the game starts
 
         Console.WriteLine("Get ready to type your fingers off with the typing game.");
         Console.WriteLine("Press the spacebar for 2 points, the right arrow key for 5 points, the letter P for 10 points, and the letter O for 15 points.");
@@ -15,15 +18,19 @@ class Program
         Console.WriteLine("If you get 200 points you win :-)");
 
         while (gameOn)
+        // All of this is only if the gameOn loop is true
         {
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            // Saves the key the user types in variable keyInfo. Console.ReadKey reads the key and (true) means it is not displayed
 
             if(keyInfo.Key == ConsoleKey.Spacebar)
+            // If the user presses the spacebar, add 2 points and display score
             {
                 score = score + 2;
                 Console.WriteLine($"Score: {score}");
             }
             else if (keyInfo.Key == ConsoleKey.RightArrow)
+            // If user presses right arrow, add 5 and display score
             {
                 score = score + 5;
                 Console.WriteLine($"Score: {score}");
@@ -31,20 +38,25 @@ class Program
             else if (keyInfo.Key == ConsoleKey.P)
             {
                 if (score < 50 && lastKey == ConsoleKey.P)
+                // If the score is under 50 and the user presses P 2+ times, the console will say stop >:(
                 {
                     Console.WriteLine("Stop");
                 }
                 else if (score >= 50)
+                // If the score is >= 50 it was increase by 10 and display
                 {
                     score = score + 10;
                     Console.WriteLine($"Score: {score}");
                 }
                 else
+                // If user presses P once this will display
                 {
                     Console.WriteLine($"I just said you need 50 points to use {keyInfo.KeyChar}");
+                    // keyInfo.KeyChar displays the key that the user just typed
                 }
             }
             else if (keyInfo.Key == ConsoleKey.O)
+            // Same thing as P but it addss 15 points if >= 50
             {
                 if (score < 50 && lastKey == ConsoleKey.O)
                 {
@@ -58,9 +70,11 @@ class Program
                 else
                 {
                     Console.WriteLine($"I just said you need 50 points to use {keyInfo.KeyChar}");
+                    // keyInfo.KeyChar saves the key the user just typed and in this case displays it
                 }
             }
             else if (keyInfo.Key == ConsoleKey.J)
+            // If user types J, subtract 3 from score. If this makes the score go negative it will stay at 0
             {
                 score = score - 3;
                 if (score < 0) score = 0;
@@ -73,24 +87,28 @@ class Program
                 Console.WriteLine($"YOU LOSE 3 POINTS (Score: {score})");
             }
             else if (keyInfo.Key == ConsoleKey.X)
+            // If user types X gameOn loop is turned too false and it stops. Also displays silly little good bye message and final score
             {
                 gameOn = false;
                 Console.WriteLine("Thank you for playing the typing game NOW GET OUTTA HEEYUH");
                 Console.WriteLine($"Final Score: {score}");
             }
             else 
+            // If user types an unspecified key, display another silly little message. keyInfo used again to display key that user just typed
             {
                 Console.WriteLine($"Did I say you could use '{keyInfo.KeyChar}'?? Try again pal");
             }
 
 
             if (score >= 200)
+            // When user reaches 200 points, gomeOn loop turns to false and stops. Display good bye message
             {
                 Console.WriteLine("You won :O good bye");
                 gameOn = false;
             }
 
             lastKey = keyInfo.Key;
+            // Update the lastKey variable to save the key the user just typed
         }
     }
 }
